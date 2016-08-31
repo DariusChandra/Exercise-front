c0486235c2d50d95766a2057831e83a0b9ea8189
Salarewor <aiko1477957@gmail.com>
2016-08-31 12:20:40 +0800

    update utils.js add getActivatedObject

--- Ajax/ch04/scripts/utils.js
+++ Ajax/ch04/scripts/utils.js
@@ -11,6 +11,26 @@ function createRequest() {
         request = null;
       }
     }
-  }	
+  }
   return request;
+}
+
+function addEventHandler(obj, eventName, handler) {
+  if (document.attachEvent) {
+    obj.attachEvent(on + eventName, handler);
+  } else if (document.addEventListener) {
+    obj.addEventListener(eventName, handler, false);
+  }
+}
+
+function getActivatedObject(e) {
+  var obj;
+  if (!e) {
+    obj = window.event.srcElement;
+  } else if (e.srcElement) {
+    obj = e.srcElement;
+  } else {
+    obj = e.target;
+  }
+  return obj;
 }
