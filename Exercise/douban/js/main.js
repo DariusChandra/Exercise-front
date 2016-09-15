0e9b0f3c43f8c5c6d159e5122e78ccccd8df8291
Salarewor <aiko1477957@gmail.com>
2016-09-15 17:26:18 +0800

    add some files

---
+++ Exercise/douban/js/main.js
@@ -0,0 +1,11 @@
+window.onload = initPage;
+
+function initPage() {
+    // var showQR = document.getElementById("showQR");
+    // showQR.onmouseover = function() {
+    //     showQR.style.display = "block";
+    // };
+    // showQR.onmouseout = function() {
+    //     showQR.style.display = "none";
+    // };  不会写。等写完页面继续写。
+}
